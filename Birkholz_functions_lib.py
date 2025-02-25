7b0c832aeab8317049cf91008a1697068d9f2fab
Alexina Birkholz <alexina@col.uwyo.edu>
2025-02-25 16:28:27 -0700

    hopefully this uploads. updated functions library as of 02/25

--- Birkholz_functions_lib.py
+++ Birkholz_functions_lib.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 def my_function (vector):
 	a=vector[0]
 	b=vector[1]
@@ -33,3 +34,20 @@ def format_axes(ax):
     ax.tick_params(axis='both', which='major', labelsize=14, length=6, width=1.5)  # Larger major ticks
     ax.tick_params(axis='both', which='minor', labelsize=12, length=3, width=1)    # Minor ticks
     ax.minorticks_on()  # Enable minor ticks
+
+def f(x):
+	for i in x:
+		result=1+0.5*(np.tanh(2*x))
+		return result
+
+def cd_approx(func, x, step_size):
+	for i in x:
+		derivative = (func(x+step_size/2)-func(x-step_size/2))/step_size - 1*1/24*step_size**2
+		return derivative
+
+def tanh_deriv_analytical(x):
+	for i in x:
+		deriv=(1/np.cosh(x))**2
+		return deriv
+#derivative is sech^2(x) but sech doesn't exist as a np function, and math functions don't work on arrays. sech = 1/cosh, so I used that instead
+
